d99630ead80f593ab2fe75f36e8011f22be4a17e
Arctyo <arctiongamz@gmail.com>
2024-10-07 19:47:46 +0200

    Updated vj1

--- Vj1/vj1.cpp
+++ Vj1/vj1.cpp
@@ -1,7 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#define MAX 1024
+
+int OpenFile(char* FileName);
+int NumberOfRows(const char*);
 
 int main(
-	printf("Hello world");
+	int numOfRows = 0;
+
+	numOfRows = NumberOfRows("imedat.txt");
 
 	return 0;
 )
+
+int NumberOfRows(const char* fileName) {
+	FILE* fp = NULL;
+	int numOfRows = 0;
+	char buffer[MAX] = {};
+
+	fp = fopen(fileName, "r");
+	if (!fp)
+	{
+		return -1;
+	}
+
+	while (!feof(fp))
+	{
+		fgets(buffer, MAX, fp);
+		numOfRows++;
+	}
+}
