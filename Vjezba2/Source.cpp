96b68c524e45df9b0e9ecde377cdd02c8419e803
gabrielhrga <gabrielhrga04@gmail.com>
2024-10-14 19:56:00 +0200

    Uploaded lab file

---
+++ Vjezba2/Source.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE 20
+
+struct _person;
+struct _person Position;
+typedef struct _person{
+	char name[MAX_SIZE];
+	char lastName[MAX_SIZE];
+	int year;
+
+	//Position next
+}Person;
+
+
+int main() {
+
+	
+
+	return 0;
+}
